b3b4bb2e28d0eef7cc0359985fa54cfa85d1d71e
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2018-05-07 12:51:56 +0200

    [INIT] init files

--- app.js
+++ app.js
@@ -1,46 +1,52 @@
-// Action creator
-const changeQuantity = (quantity) => {
-	return {
-  	type: 'QUANTITY_CHANGED',
-    payload: quantity
-  }
+const startApp = () => {
+    // Action creator
+    const changeQuantity = (quantity) => {
+        return {
+        type: 'QUANTITY_CHANGED',
+        payload: quantity
+      }
+    };
+
+    // Reducer
+    const totalReducer = (state = 0, action) => {
+      switch (action.type) {
+        case 'QUANTITY_CHANGED':
+          return '';
+        default:
+          return state;
+      }
+    };
+
+    // Combine reducers
+    const rootReducer = Redux.combineReducers({
+      total: totalReducer
+    });
+
+    // Action Dispatch
+    const $quantityButton = document.querySelector('.quantity');
+
+    $quantityButton.addEventListener('oninput', (event) => {
+        const quantity = event.currentTarget.value;
+
+      store.dispatch(changeQuantity(quantity));
+    });
+
+    // Listen Store changes - Subscribe
+    const updateDomTotal = () => {
+        const state = store.getState();
+      console.log('state', state);
+      document.querySelector('.total').innerHTML = state.total;
+    };
+
+    const store = Redux.createStore(
+        rootReducer,
+      {},
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+
+    store.subscribe(updateDomTotal);
 };
 
-// Reducer
-const totalReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'QUANTITY_CHANGED':
-      return '';
-    default:
-      return state;
-  }
-}
-
-// Combine reducers
-const rootReducer = Redux.combineReducers({
-  total: totalReducer
-})
-
-// Action Dispatch
-const $quantityButton = document.querySelector('.quantity');
-
-$quantityButton.addEventListener('click', (event) => {
-	const quantity = event.currentTarget.value;
-
-  store.dispatch(changeQuantity(quantity));
-});
-
-// Listen Store changes - Subscribe
-const updateDomTotal = () => {
-	const state = store.getState();
-  console.log('state', state);
-  document.querySelector('.total').innerHTML = state.total;
-}
-
-const store = Redux.createStore(
-	rootReducer,
-  {},
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-store.subscribe(updateDomTotal);
+(function() {
+    startApp();
+})();
